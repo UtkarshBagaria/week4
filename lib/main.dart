c8f7ce5770f37228da1073c4654947dc60cf1487
Utkarsh <utkarshbagaria3008@gmail,com>
2021-03-08 19:42:40 +0530

    6-11:50

--- lib/main.dart
+++ lib/main.dart
@@ -56,6 +56,63 @@ class _PomodoroState extends State<Pomodoro> {
                   ),
                 ),
               ),
+              SizedBox(height: 30.0,),
+              Expanded(child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white60,
+                  borderRadius: BorderRadius.only(topRight: Radius.circular(30.0),topLeft: Radius.circular(30.0),),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.only(top: 30.0,left: 20.0,right: 20.0,),
+                  child: Column(
+                    children: <Widget>[
+                      Expanded(child: Row(
+                        children: <Widget>[
+                          Expanded(child: Column(
+                            children: <Widget> [
+                              Text(
+                                "Study Timer",
+                                style: TextStyle(fontSize: 30.0,),
+                              ),
+                              SizedBox(height:10.0,),
+                              Text("25",style: TextStyle(fontSize: 80.0,),),
+                            ],
+                          ),
+                          ),
+                          Expanded(child: Column(
+                            children: <Widget> [
+                              Text(
+                                "Pause Timer",
+                                style: TextStyle(fontSize: 30.0,),
+                              ),
+                              SizedBox(height:10.0,),
+                              Text("5",style: TextStyle(fontSize: 80.0,),),
+                            ],
+                          ),
+                          ),
+
+                        ],
+
+                      ),
+                      ),
+                      Padding(padding: EdgeInsets.symmetric(vertical:28.0),
+                        child: RaisedButton(
+                        color: Colors.blue,
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100.0),
+                        ),
+                        child: Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Text("Start Studying",
+                        style: TextStyle(color: Colors.white60,
+                        fontSize: 22.0,),),),
+                        ) ,
+                        ),
+
+                    ],
+                  ),
+                ),
+              ),),
             ],
           ),
         ),
