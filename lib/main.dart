0923519d34bfd90d4da35bb999a455cc73411c07
Vishakha <vishakhahegde23@gmail.com>
2021-03-08 11:07:51 +0530

    commit1 basic frontend

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 void main() {
-  runApp(MyApp());
-}//comments
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Pomodoro(),
+  ));
+}
 
+class Pomodoro extends StatefulWidget {
+  @override
+  _PomodoroState createState() => _PomodoroState();
+}
 
+class _PomodoroState extends State<Pomodoro> {
+  double percent = 0;
+  static int TimeInMinut = 25;
+  int TimeInSec = TimeInMinut * 60;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Color(0xff006064), Color(0xff80deea)],
+                begin: FractionalOffset(
+                  0.5,
+                  1,
+                )),
+          ),
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 30.0),
+                child: Text(
+                  "Pomodoro Clock",
+                  style: TextStyle(color: Colors.black, fontSize: 40.0),
+                ),
+              ),
+              Expanded(
+                child: CircularPercentIndicator(
+                  percent: percent,
+                  animation: true,
+                  animateFromLastPercent: true,
+                  radius: 250.0,
+                  lineWidth: 20.0,
+                  progressColor: Colors.white,
+                  center: Text(
+                    "$TimeInMinut",
+                    style: TextStyle(color: Colors.black, fontSize: 80.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
